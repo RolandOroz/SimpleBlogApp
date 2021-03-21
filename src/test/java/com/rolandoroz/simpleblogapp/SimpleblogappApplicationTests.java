f883fcde243564137fcfb0e8daf4c5575f862409
D13 <rolandoroz@gmail.com>
2021-03-21 21:44:33 +0100

    Basic Spring config

---
+++ src/test/java/com/rolandoroz/simpleblogapp/SimpleblogappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandoroz.simpleblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleblogappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
